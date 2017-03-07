233a29ffe53d31cd03c7f2e172707deab26474ba
Ryann Micua (office) <rmicua@ssd.org>
2017-03-07 11:54:40 +0800

    remove optimization of config files on run.sh

--- run.sh
+++ run.sh
@@ -11,9 +11,9 @@ then
         chown bareos /var/lib/bareos/storage
 
         # optimize configuration
-        export BUILD_NAME=$(grep -iR -m 1 "Name = " /etc/bareos/bareos-sd.conf | awk '{ split($3, x, "-"); print x[1] }')
+        #export BUILD_NAME=$(grep -iR -m 1 "Name = " /etc/bareos/bareos-sd.conf | awk '{ split($3, x, "-"); print x[1] }')
 
-        sed -i "s/$BUILD_NAME-\(dir\|sd\|fd\|mon\)/bareos-\1/" /etc/bareos/bareos-sd.conf
+        #sed -i "s/$BUILD_NAME-\(dir\|sd\|fd\|mon\)/bareos-\1/" /etc/bareos/bareos-sd.conf
 
         # cleanup        
         touch /etc/firstrun
